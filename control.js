35a0d9b7e159b989b111e757eabb58bb279d3f2e
ufahmed <ahmedsellami375@gmail.com>
2023-10-10 18:34:53 +0100

    first commit

---
+++ control.js
@@ -0,0 +1,15 @@
+function logout(){
+    var x=document.getElementById("login");
+    x.innerText="logout";
+}
+
+
+function hide(){
+    var y=document.getElementById("definition");
+    y.remove();        
+
+
+}
+function affiche(){
+    alert("ninja was liked");
+}
